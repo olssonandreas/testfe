9c23593e48b50969b6424fd27a7a187583294740
Andreas Olsson <hello@andreas-olsson.se>
2015-01-16 03:04:32 +0100

    updated

--- js/custom.js
+++ js/custom.js
@@ -15,6 +15,25 @@ $(function(){
 
     if ($("search-box:first").is(":focus")) {
 }
-
     
+});
+
+
+
+
+$('.carousel2[data-type="multi"] .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
 });
