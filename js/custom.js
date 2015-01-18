c2e2e74b866d888ef2e30f9a163f78527acc1df4
Andreas Olsson <hello@andreas-olsson.se>
2015-01-18 21:15:27 +0100

    update

--- js/custom.js
+++ js/custom.js
@@ -29,26 +29,3 @@ $('.carousel2[data-type="multi"] .item').each(function(){
 
 
 
-$(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object/2 ){
-                
-                $(this).animate({'opacity':'1'},800);
-                    
-            }
-            
-        }); 
-    
-    });
-    
-});
